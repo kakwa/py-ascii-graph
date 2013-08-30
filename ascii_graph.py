75d1ac3628de55b04103369401be1cfefd251edf
xiongchiamiov <xiong.chiamiov@gmail.com>
2013-08-30 16:22:38 -0700

    Allow importing as a module.
    1. Don't do a test print on when the file is imported.
    2. Don't use dashes in the filename, since Python modules can only use
       alphanumeric characters and underscores.

--- ascii_graph.py
+++ ascii_graph.py
@@ -131,7 +131,8 @@ class Pyasciigraph:
 
         return result
 
-test=[('long_label', 423), ('sl', 1234), ('line3', 531), ('line4', 200), ('line5', 834)]
-graph=Pyasciigraph()
-for line in  graph.graph('test print', test):
-    print line
+if __name__ == '__main__':
+    test=[('long_label', 423), ('sl', 1234), ('line3', 531), ('line4', 200), ('line5', 834)]
+    graph=Pyasciigraph()
+    for line in  graph.graph('test print', test):
+        print line
