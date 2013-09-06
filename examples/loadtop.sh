9935cc26d0c637b0e86429ec3b77161d8488da18
kakwa <carpentier.pf@gmail.com>
2013-09-06 19:55:54 +0200

    adding a small example shell script

---
+++ examples/loadtop.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+#get the number of proc (empiricaly I always try to have load < number of cores)
+max=`cat /proc/cpuinfo|grep processor|wc -l`
+
+while true
+do
+    #get the load
+    load_15=`uptime|sed "s/.*:\ //"|cut -d ',' -f 3`
+    load_1=`uptime|sed "s/.*:\ //"|cut -d ',' -f 1`
+    load_5=`uptime|sed "s/.*:\ //"|cut -d ',' -f 2`
+
+    #get the width
+    width=`tput cols`
+
+    #get the number of char to rewrite
+    char=$(( $width * 5 + 7 ))
+    printf "load threshold:$max\nload last minute:$load_1\nload last 5 minutes:$load_5\nload last 15 minutes:$load_15\n"|asciigraph -w $width -l 'loadtop'
+
+    #going back to where we start writing (overwrite the lines)
+    counter=0
+    while [ $counter -ne $char ]
+    do
+        printf "\b"
+        counter=$(( $counter + 1  ))
+    done
+    #refresh every 1 second
+    sleep 1
+done
