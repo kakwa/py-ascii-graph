774ff409eac1f8512f4e7838b7828de21ecfd90f
kakwa <carpentier.pf@gmail.com>
2013-09-06 20:52:28 +0200

    cleaner way to rewrite output

--- examples/loadtop.sh
+++ examples/loadtop.sh
@@ -3,6 +3,10 @@
 # It's a small example of asciigraph usage
 # It just displays the load
 
+tput clear
+old_width=`tput cols`
+
+
 #get number of cores (empiricaly I always try to have load < number of cores)
 max=`cat /proc/cpuinfo|grep processor|wc -l`
 
@@ -15,23 +19,21 @@ do
 
     #get the width
     width=`tput cols`
+    if [ $width -ne $old_width ]
+    then
+        tput clear
+    fi
+    old_width=$width
 
     #get the number of char to rewrite
-    char=$(( $width * 5 + 7 ))
+   # char=$(( $width * 5 + 7 ))
     printf "\
 load threshold:$max\n\
 load last minute:$load_1\n\
 load last 5 minutes:$load_5\n\
 load last 15 minutes:$load_15\n" | asciigraph -w $width -l 'loadtop'
 
-    #going back to where we start writing (overwrite the lines)
-    counter=0
-    while [ $counter -ne $char ]
-    do
-        printf "\b"
-        counter=$(( $counter + 1  ))
-    done
-
     #refresh every 1 second
+    tput cup 0 0
     sleep 1
 done
