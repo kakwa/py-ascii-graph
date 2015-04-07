4361beb6fd4aeb9f4811150f4750851a674b4bd3
kakwa <carpentier.pf@gmail.com>
2015-04-07 23:13:00 +0200

    using ascii_graph.colors in example

--- examples/color_graphs.py
+++ examples/color_graphs.py
@@ -1,34 +1,34 @@
 #!/bin/env python2
 
 from ascii_graph import Pyasciigraph
+from ascii_graph.colors import *
 
-class bcolors:
-    HEADER = '\033[95m'
-    OKBLUE = '\033[94m'
-    OKGREEN = '\033[92m'
-    WARNING = '\033[93m'
-    FAIL = '\033[91m'
-    ENDC = '\033[0m'
-    BOLD = '\033[1m'
-    UNDERLINE = '\033[4m'
-    Red = "\x1b[31m"
-    Bold_Red = "\x1b[31;1m"
-    Green = "\x1b[32m"
-    Bold_Green = "\x1b[32;1m"
-    Yellow = "\x1b[33m"
-    Bold_Yellow = "\x1b[33;1m"
-    Blue = "\x1b[34m"
-    Bold_Blue = "\x1b[34;1m"
-    Magenta = "\x1b[35m"
-    Bold_Magenta = "\x1b[35;1m"
-    Cyan = "\x1b[36m"
-    Bold_Cyan = "\x1b[36;1m"
-    White = "\x1b[37"
-    Bold_White = "\x1b[37;1m"
+test = [('testval0', 600),
+        ('testval1', 500, Pur),
+        ('testval2', 400, Red),
+        ('testval3', 400, Red),
+        ('testval4', 300, Gre),
+        ('testval5', 200, Yel),
+        ('testval6', 100, Cya),
+        ('testval7', 50,  Blu) ]
 
-test = [ ('testval0', 600), ('testval1', 400, bcolors.Red), ('testval2', 300, bcolors.Green), ('testval3', 200, bcolors.Yellow), ('testval2', 100, bcolors.Cyan), ('testval3', 50, bcolors.Blue) ]
 graph = Pyasciigraph()
 for line in graph.graph('test graph', test):
     print(line)
+
+test = [('testval0', 142),
+        ('testval1', 204, BPur),
+        ('testval2', 501, URed),
+        ('testval3', 103, IRed),
+        ('testval4',  29, BIGre),
+        ('testval5',  19, UYel),
+        ('testval6',  99, ICya),
+        ('testval7', 404, BBlu)]
+
+graph = Pyasciigraph()
+for line in graph.graph('test graph', test):
+    print(line)
+
+
 
 exit(0)
