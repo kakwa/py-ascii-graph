c6c3f22b4012985c4e36f6864dbfa25b6a4a88df
David Whiteside <david.whiteside@nrel.gov>
2015-04-07 16:20:21 -0600

    refactor - cleaned up to make the test list more readable

--- examples/color_multivalue_graphs.py
+++ examples/color_multivalue_graphs.py
@@ -4,15 +4,12 @@ from ascii_graph import Pyasciigraph
 from ascii_graph.colors import *
 
 test = [('testval0', 600),
-        ('testval1', 400, Red),
-	('testval2', [(300, Gre), (500, Blu)]),
-        ('testval3', [(200, Yel), (100,)]),
-	('testval4', 100, Cya),
-	('testval5', 50, Blu),
-        ('testval6', [(100, Gre),
-	(150, Red),
-	(200, Yel),
-	(600, Blu)])]
+           ('testval1', 400, Red),
+           ('testval2', [(300, Gre), (500, Blu)]),
+           ('testval3', [(200, Yel), (100,)]),
+           ('testval4', 100, Cya),
+           ('testval5', 50, Blu),
+           ('testval6', [(100, Gre), (150, Red), (200, Yel), (600, Blu)])]
 graph = Pyasciigraph(separator_length=4)
 for line in graph.graph('test graph', test):
     print(line)
