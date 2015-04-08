a0552a03c18e593bfc7045b03111ec196b838768
kakwa <carpentier.pf@gmail.com>
2015-04-09 00:20:40 +0200

    rework a little the examples

--- examples/graphs.py
+++ examples/graphs.py
@@ -2,25 +2,32 @@ from ascii_graph import Pyasciigraph
 
 test = [('long_label', 423), ('sl', 1234), ('line3', 531), ('line4', 200), ('line5', 834)]
 graph = Pyasciigraph()
+
+# Simpler example
 for line in graph.graph('test 1', test):
     print(line)
 
-#take also anything castable to utf-8
+# Take also anything castable to utf-8
 test = [(1, 423), (2, 1234), (3, 531), ('line4', 200), ('line5', 834)]
 for line in graph.graph('test 2', test):
     print(line)
 
-#example of sort
+# Example of sort
 test.sort(reverse=False, key=lambda tup: tup[1] )
 for line in graph.graph('test 2', test):
     print(line)
 
-#example of reverse sort
+# Example of reverse sort
 test.sort(reverse=True, key=lambda tup: tup[1] )
 for line in graph.graph('test 2', test):
     print(line)
 
 print('')
-#No label
+# No label
+for line in graph.graph(data=test):
+    print(line)
+
+# Different separator length
+graph = Pyasciigraph(separator_length=4)
 for line in graph.graph(data=test):
     print(line)
