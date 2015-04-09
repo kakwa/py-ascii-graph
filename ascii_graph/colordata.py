1a453c1fecd02abc3f5f593765d1ad7df08caf02
kakwa <carpentier.pf@gmail.com>
2015-04-09 08:35:15 +0200

    fix docstring in vcolor

--- ascii_graph/colordata.py
+++ ascii_graph/colordata.py
@@ -1,10 +1,13 @@
 def vcolor(data, pattern):
     """ Color a graph line by line
+
     :param data: the data
     :type data: list of tuples (info, value)
     :param pattern: list of colors, this list defines
       the pattern to color each line of the graph.
     :type pattern: list of 'colors' (str)
+    :return: the colored graph
+    :rtype: list of arrays (<info>, <value>, <color>)
     """
     ret=[]
     l=len(pattern)
