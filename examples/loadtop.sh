28e6137adf99aa912da91bde56d79ffc21d5f5f5
Carpentier Pierre-Francois <carpentier.pf@gmail.com>
2015-04-09 13:47:49 +0200

    Update loadtop.sh

--- examples/loadtop.sh
+++ examples/loadtop.sh
@@ -7,19 +7,23 @@ tput clear
 old_width=`tput cols`
 
 
-#get number of cores (empiricaly I always try to have load < number of cores)
+# Get number of cores (empiricaly I always try to have load < number of cores)
 max=`grep -c processor /proc/cpuinfo`
+# Calculate the first threshold (until this threshold, graph is green)
 t1=`awk "BEGIN {print $max * 3 / 4}"`
+# Calculate the second threshold.
+# Between t1 and t2, graph is yellow. 
+# After t2, graph is red.
 t2=`awk "BEGIN {print $max * 4 / 4}"`
 
 while true
 do
-    #get the load
+    # Get the load
     load_15=`uptime|sed "s/.*:\ //"|cut -d ',' -f 3`
     load_1=`uptime|sed "s/.*:\ //"|cut -d ',' -f 1`
     load_5=`uptime|sed "s/.*:\ //"|cut -d ',' -f 2`
 
-    #get the width
+    # Get the width of the term
     width=`tput cols`
     if [ $width -ne $old_width ]
     then
@@ -27,8 +31,6 @@ do
     fi
     old_width=$width
 
-    #get the number of char to rewrite
-   # char=$(( $width * 5 + 7 ))
     printf "\
 load threshold:$max\n\
 load last minute:$load_1\n\
