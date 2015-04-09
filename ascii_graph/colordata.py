b9db46b9090be0ea51edcf7c6858d8d88889c779
kakwa <carpentier.pf@gmail.com>
2015-04-09 08:26:06 +0200

    adding vcolor functionnality
    vcolor adds color to data according to a pattern.
    Each lines are colored according to this pattern.

---
+++ ascii_graph/colordata.py
@@ -0,0 +1,16 @@
+def vcolor(data, pattern):
+    """ Color a graph line by line
+    :param data: the data
+    :type data: list of tuples (info, value)
+    :param pattern: list of colors, this list defines
+      the pattern to color each line of the graph.
+    :type pattern: list of 'colors' (str)
+    """
+    ret=[]
+    l=len(pattern)
+    c=0
+    for info,value in data:
+        ret.append((info, value, pattern[c]))
+        c = (c + 1) % l 
+    return ret
+
