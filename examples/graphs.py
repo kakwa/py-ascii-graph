2b1f6aa3af6e20e9e133e27b2e6f4f41a29a9f71
Carpentier Pierre-Francois <carpentier.pf@gmail.com>
2015-04-10 08:05:22 +0200

    Update graphs.py

--- examples/graphs.py
+++ examples/graphs.py
@@ -1,6 +1,8 @@
 from ascii_graph import Pyasciigraph
 
-test = [('long_label', 423), ('sl', 1234), ('line3', 531), ('line4', 200), ('line5', 834)]
+test = [('long_label', 423), ('sl', 1234), ('line3', 531),
+    ('line4', 200), ('line5', 834)]
+
 graph = Pyasciigraph()
 
 # Simpler example
