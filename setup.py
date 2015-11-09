549aa7d5ec3dbe98d4ce575ffff30e78b9a10fcb
kakwa <carpentier.pf@gmail.com>
2015-11-09 23:06:11 +0100

    prepare version bump

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ except ImportError:
 
 setup(
     name = 'ascii_graph',
-    version = '1.0.1',
+    version = '1.1.0',
     author = 'Pierre-Francois Carpentier',
     author_email = 'carpentier.pf@gmail.com',
     packages = ['ascii_graph'],
