0e393736e8b268d71f585d7ddc14ede88ba9e69b
kakwa <carpentier.pf@gmail.com>
2015-11-10 00:05:27 +0100

    better comments in gdu.sh example

--- examples/gdu.sh
+++ examples/gdu.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# simple example to graph du output
+# simple example to graph the du command output
 
 exit_err(){
     msg=$1
@@ -8,11 +8,15 @@ exit_err(){
     exit 1
 }
 
+# default dir to study
 dir='./'
+# taking all the arguments as one
 ! [ -z "$@" ] && dir="$@"
 
+# exit if argument is not a directory
 [ -e "$dir" ] || exit_err "Directory '$dir' doesn't exist"
 
+# run du -> refomart a little with awk -> give it to asciigraph
 du -d 1 "$dir" | \
-    awk '{printf "%s:%s\n", $2, $1 * 1024;}' |\
+    awk '{printf "%s:%s\n", $2, $1 * 1024;}' | \
     asciigraph -l 'Disk Usage' -H -s dec
