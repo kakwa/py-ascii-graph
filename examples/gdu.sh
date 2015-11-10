74e0ebe1af385f12691d9d0704e6b076edf9dfdf
kakwa <carpentier.pf@gmail.com>
2015-11-10 22:46:57 +0100

    fixes and modification on the gdu.sh examples
    different argument handling and fix on space in file names

--- examples/gdu.sh
+++ examples/gdu.sh
@@ -4,21 +4,7 @@ width=`tput cols`
 
 # simple example to graph the du command output
 
-exit_err(){
-    msg=$1
-    printf "$msg\n"
-    exit 1
-}
-
-# default dir to study
-dir='./'
-# taking all the arguments as one
-! [ -z "$@" ] && dir="$@"
-
-# exit if argument is not a directory
-[ -e "$dir" ] || exit_err "Directory '$dir' doesn't exist"
-
 # run du -> refomart a little with awk -> give it to asciigraph
-du -d 1 "$dir" | \
-    awk '{printf "%s:%s\n", $2, $1 * 1024;}' | \
+du -d 1 $@ | head -n -1 |\
+    awk '{s = ""; for (i = 2; i <= NF; i++) s = s $i " "; printf "%s:%s\n", s, $1 * 1024;}' | \
     asciigraph -l 'Disk Usage' -H -s dec -c -w $width
